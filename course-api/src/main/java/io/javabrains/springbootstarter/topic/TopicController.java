16d37d4d31680a35eb53aab23371a0bb428d7737
shubh <shubh@KRUNAL>
2018-01-23 23:10:44 -0500

    Spring Boot Tutorial

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,23 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopic() {
+		
+		return Arrays.asList(
+				new Topic("Spring" , "Spring Framwork" , "Spring framework discription"),
+				new Topic("KRunal" , "KK Framwork" , "KK framework discription"),
+				new Topic("AArav" , "AArsh Framwork" , "Shubhechha framework discription")
+				
+				);
+	}
+
+}
